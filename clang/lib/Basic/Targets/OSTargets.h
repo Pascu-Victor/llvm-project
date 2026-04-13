0a0bedad7b3589887fcc255d1055116d100f324d
Pascu-Victor <me@vpascu.ro>
2026-04-13 12:17:59 +0300

    add _DEFAULT_SOURCE to WOS driver

--- clang/lib/Basic/Targets/OSTargets.h
+++ clang/lib/Basic/Targets/OSTargets.h
@@ -323,10 +323,10 @@ protected:
     Builder.defineMacro("__WOS__");
     Builder.defineMacro("__ELF__");
     DefineStd(Builder, "unix", Opts);
+    Builder.defineMacro("_GNU_SOURCE");
+    Builder.defineMacro("_DEFAULT_SOURCE");
     if (this->HasFloat128)
       Builder.defineMacro("__FLOAT128__");
-    if (Opts.CPlusPlus)
-      Builder.defineMacro("_GNU_SOURCE");
   }
 
 public:
